f8f85e4b4bc8b572c79472ae1f6be3c4775a0448
andreatruchot <andytrutsh@yahoo.fr>
2024-09-26 15:26:25 +0200

    changement des commentaires dans validateEmailService

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,28 +1,35 @@
+// Importation du modèle User depuis le fichier ../models/users
 const User = require('../models/users'); 
-
+// Déclaration de la fonction middleware d'authentification
 const authenticate = async (req, res, next) => {
 
     // Extrait le token d'authentification de l'en-tête 'Authorization' de la requête
 // L'en-tête 'Authorization' est normalement formaté comme 'Bearer <token>',
 // donc on divise la chaîne sur l'espace (' ') et on prend le deuxième élément ([1]),
   const token = req.headers.authorization?.split(' ')[1];
-  
+
+  // Vérifie si le token est absent
   if (!token) {
+
+    // Retourne une réponse 401 (Unauthorized) avec un message d'erreur
     return res.status(401).json({ message: 'Authentication token is missing' });
   }
 
   try {
+    // Cherche un utilisateur dans la base de données avec le token fourni
     const user = await User.findOne({ token: token });
 
     if (!user) {
+      // Si aucun utilisateur n'est trouvé, retourne une réponse 404 (Not Found) avec un message d'erreur
       return res.status(404).json({ message: 'User not found with provided token' });
     }
-
+    // Ajoute l'ID de l'utilisateur à l'objet de requête pour une utilisation ultérieure
     req.userId = user._id.toString(); 
     next();
   } catch (error) {
+     // En cas d'erreur (comme un token invalide), retourne une réponse 401 (Unauthorized) avec un message d'erreur
     res.status(401).json({ message: 'Invalid token' });
   }
 }
-
+// Exporte la fonction middleware authenticate pour une utilisation dans d'autres parties de l'application
 module.exports = authenticate;
--- services/validateEmailService.js
+++ services/validateEmailService.js
@@ -5,14 +5,12 @@ const dns = require('dns');
  * Valide le format de l'adresse email fournie.
  * @param {string} email L'adresse email à valider.
  * @returns {boolean} Retourne `true` si l'email est dans un format valide, sinon `false`.
- * Utilise une expression régulière (regex) pour vérifier le format.
- * La regex vérifie que l'email :
- * - Ne commence pas et ne finit pas par des espaces ou un '@'.
- * - Contient un '@' suivi par au moins un caractère qui n'est pas un espace ou '@',
- *   suivi d'un '.', et ensuite d'autres caractères qui ne sont pas des espaces ou '@'.
  */
+
 function validateEmailFormat(email) {
+  // Expression régulière (regex) pour valider le format de l'email
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   // Teste si l'email correspond au format défini par la regex
   return regex.test(email);
 }
 
@@ -20,18 +18,19 @@ function validateEmailFormat(email) {
  * Vérifie si le domaine de l'adresse email peut recevoir des emails en cherchant ses enregistrements MX.
  * @param {string} email L'adresse email dont le domaine sera vérifié.
  * @returns {Promise<boolean>} Une promesse qui résout en `true` si le domaine peut recevoir des emails, sinon `false`.
- * La fonction extrait d'abord le domaine de l'email en séparant la chaîne sur '@' et en prenant la partie après.
- * Ensuite, elle utilise `dns.resolveMx` pour rechercher les enregistrements MX du domaine.
- * Si la recherche réussit et renvoie au moins un enregistrement MX, la promesse est résolue avec `true`.
- * En cas d'erreur ou si aucun enregistrement MX n'est trouvé, la promesse est rejetée ou résolue avec `false`.
  */
 function checkDomain(email) {
+  // Retourne une promesse pour gérer l'asynchronisme
   return new Promise((resolve, reject) => {
+      // Extraction du domaine à partir de l'email (la partie après le '@')
     const domain = email.split('@')[1];
     dns.resolveMx(domain, (err, addresses) => {
+       // Résolution DNS pour chercher les enregistrements MX du domaine
       if (err) {
+         // En cas d'erreur, la promesse est rejetée
         reject('Erreur lors de la résolution du domaine.');
       } else {
+         // Si des enregistrements MX sont trouvés, la promesse est résolue avec `true`
         resolve(addresses && addresses.length > 0);
       }
     });
